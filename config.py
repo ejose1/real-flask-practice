4cc06705741f307aa9664e866bdb7ec5b73aac7b
Edward Jose <eddddd@localhost.localdomain>
2017-05-03 20:43:03 -0500

    First commit

---
+++ config.py
@@ -0,0 +1,10 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com'},
+    {'name': 'Yahoo', 'url': 'https://www.yahoo.com'},
+    {'name': 'Aol', 'url': 'https://www.aol.com'},
+    {'name': 'Flicr', 'url': 'https://flickr.com'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+    
